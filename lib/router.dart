9e01f50c19dabc71988bb899798e275ec0565ba9
lcw1030 <lcw1030@korea.ac.kr>
2021-07-12 15:57:42 +0900

    delete second.dart

--- lib/router.dart
+++ lib/router.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weatherweather/screen.dart';
 import 'package:weatherweather/defaultScreen.dart';
 
-import 'package:weatherweather/Second.dart';
+import 'package:weatherweather/screens/temp.dart';
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch(settings.name) {
@@ -11,10 +11,10 @@ class Router {
           settings: RouteSettings(name: settings.name),
           builder: (_) => Screen(),
         );
-      case '/second':
+      case '/temp':
         return MaterialPageRoute(
             settings: RouteSettings(name: settings.name),
-            builder: (_) => Second()
+            builder: (_) => Temp()
         );
       default:
         return MaterialPageRoute(builder: (context) => defaultScreen());
